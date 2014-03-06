865c4c86a060e721951983176d16e1554bb286ea
Gratien D'haese <gratien.dhaese@hp.com>
2014-03-06 13:33:35 +0100

    put the year at the end (need for GNU/Linux!)

--- opt/upgrade-ux/scripts/prep/default/05_select_ini_file.sh
+++ opt/upgrade-ux/scripts/prep/default/05_select_ini_file.sh
@@ -2,9 +2,9 @@
 # read and analyze the $ETC_DIR/$OS-$OS_VERSION.ini file
 if [[ -z "$INI_FILE" ]]; then
     case "$OS" in
-        "HP-UX"|"SunOS") INI_FILE="$ETC_DIR/$(date '+%Y')-${OS}-${OS_VERSION}.ini" ;;
-        "GNU/Linux")     INI_FILE="$ETC_DIR/$(date '+%Y')-${OS}-${OS_VENDOR}-${OS_VERSION}.ini" ;;
-        *)               INI_FILE="$ETC_DIR/$(date '+%Y')-${OS}-${OS_VENDOR}-${OS_VERSION}.ini" ;;
+        "HP-UX"|"SunOS") INI_FILE="$ETC_DIR/${OS}-${OS_VERSION}-$(date '+%Y').ini" ;;
+        "GNU/Linux")     INI_FILE="$ETC_DIR/${OS}-${OS_VENDOR}-${OS_VERSION}-$(date '+%Y').ini" ;;
+        *)               INI_FILE="$ETC_DIR/${OS}-${OS_VENDOR}-${OS_VERSION}.ini" ;;
     esac
 fi
 if [[ -f $INI_FILE ]]; then
