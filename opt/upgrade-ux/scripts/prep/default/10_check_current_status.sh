de8675adf6565227e838a0011ea4d68962c122b9
Gratien D'haese <gratien.dhaese@hp.com>
2014-03-06 14:59:05 +0100

    add status script (we will append the <date> <time> stage:<start|phase1|...|end> and read the CURRENT_STATUS (tail -1 of the $VAR_DIR/status file)

---
+++ opt/upgrade-ux/scripts/prep/default/10_check_current_status.sh
@@ -0,0 +1,16 @@
+# 10_check_current_status.sh
+# purpose: read the content of file $VAR_DIR/status which contain flags where we are in the upgrade process
+# -------  as we may reboot and must be able to restart at the point where we left off
+# the status of the last will be stored in variable CURRENT_STATUS
+# at each phase in the process we will change the value of CURRENT_STATUS
+
+if [[ ! -f $VAR_DIR/status ]]; then
+    Log "Initialize $VAR_DIR/status with status \"$stage:start\""
+    echo "$(Stamp)$stage:start" > $VAR_DIR/status
+    CURRENT_STATUS="$stage:start"
+else
+    CURRENT_STATUS=$(cat $VAR_DIR/status | awk '{print $3}')
+fi
+
+LogPrint "Current status is $CURRENT_STATUS"
+
