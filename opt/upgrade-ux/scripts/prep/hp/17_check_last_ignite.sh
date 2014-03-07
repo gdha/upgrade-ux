a6b0cb48e1fe438ea10ba285d6190180248bd509
Gratien D'haese <gratien.dhaese@hp.com>
2014-03-07 15:54:29 +0100

    script to verify if ignite/ux backup was made on hp

---
+++ opt/upgrade-ux/scripts/prep/hp/17_check_last_ignite.sh
@@ -0,0 +1,12 @@
+# 17_check_last_ignite.sh
+#########################
+# verify if this system has a recent ignite/ux image backup
+
+if [[ ! -f /var/opt/ignite/recovery/previews ]]; then
+    Error "No Ignite/UX image backup was made of this system. Act now."
+fi
+
+# save a copy of this file
+cp  /var/opt/ignite/recovery/previews $VAR_DIR/ignite.previews
+Log "Last Ignite/UX image backup was made on $(grep last_preview $VAR_DIR/ignite.previews | cut -d= -f2)"
+Log "All info on Ignite/UX is stored in $VAR_DIR/ignite.previews"
