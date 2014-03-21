e0f2803b0a08a21bdf61d15f18c7ff9efddc6565
Gratien D'haese <gratien.dhaese@hp.com>
2014-03-21 10:24:27 +0100

    update, add comment in 10_check_current_status.sh

--- opt/upgrade-ux/scripts/prep/default/10_check_current_status.sh
+++ opt/upgrade-ux/scripts/prep/default/10_check_current_status.sh
@@ -1,15 +1,17 @@
 # 10_check_current_status.sh
 # purpose: read the content of file $VAR_DIR/status which contain flags where we are in the upgrade process
 # -------  as we may reboot and must be able to restart at the point where we left off
-# the status of the last will be stored in variable CURRENT_STATUS
-# at each phase in the process we will change the value of CURRENT_STATUS
+# the status of the last stage will be stored in variable CURRENT_STATUS (appending)
+# at each phase in the process we will change the value of CURRENT_STATUS ($stage:[start|ended])
+# flag looks like e.g.: 2014-03-20 13:23:59 prep:ended
 
 if [[ ! -f $VAR_DIR/status ]]; then
+    # first time we ever run this tool
     Log "Initialize $VAR_DIR/status with status \"$stage:start\""
     echo "$(Stamp)$stage:start" > $VAR_DIR/status
     CURRENT_STATUS="$stage:start"
 else
-    # read the last line of the status file (as we will append our data)
+    # read the last line of the status file (as we will append our status file)
     CURRENT_STATUS=$(tail -1 $VAR_DIR/status | awk '{print $3}')
 fi
 
