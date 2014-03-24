35baad58ef326b82affaf08f29420e7991d20bc1
Gratien D'haese <gratien.dhaese@hp.com>
2014-03-24 16:06:41 +0100

    improved the comments a bit (no code has changed)

--- opt/upgrade-ux/scripts/prep/default/06_check_preview_run.sh
+++ opt/upgrade-ux/scripts/prep/default/06_check_preview_run.sh
@@ -1,6 +1,6 @@
 # 06_check_preview_run.sh
 # we will enforce a preview run (even when we forgot it with '-p' option!)
-# we want, at all cost, to avoid any unplesant suprises
+# we want, at all cost, to avoid any unplesant surprises
 if [[ ! -f $PREVIEW_FILE ]]; then
     date > $PREVIEW_FILE
     PREVIEW_MODE=1
--- opt/upgrade-ux/scripts/prep/default/10_check_current_status.sh
+++ opt/upgrade-ux/scripts/prep/default/10_check_current_status.sh
@@ -1,7 +1,8 @@
 # 10_check_current_status.sh
 # purpose: read the content of file $STATUS_FILE which contain flags where we are in the upgrade process
 # -------  as we may reboot and must be able to restart at the point where we left off
-# the status of the last stage will be stored in variable CURRENT_STATUS (appending)
+#          The status will be appending to the $STATUS_FILE. 
+# the status of the last stage will be stored in variable CURRENT_STATUS
 # at each phase in the process we will change the value of CURRENT_STATUS ($stage:[start|ended])
 # flag looks like e.g.: 2014-03-20 13:23:59 prep:ended
 
