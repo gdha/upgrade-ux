082e8a53e398052b83cd0223379ac7bb1e873d5c
Gratien D'haese <gratien.dhaese@hp.com>
2014-03-25 12:50:41 +0100

    moved the BUILD_DIR and TMP_DIR to its own script

---
+++ opt/upgrade-ux/scripts/prep/default/03_prepare_tmp_build_area.sh
@@ -0,0 +1,14 @@
+# 03_prepare_tmp_build_area.sh
+# 
+# create temporary work area and register removal exit task
+BUILD_DIR="$(mktemp)"
+mkdir -p $BUILD_DIR
+StopIfError "Could not create build area '$BUILD_DIR'"
+QuietAddExitTask cleanup_build_area_and_end_program
+
+TMP_DIR=$BUILD_DIR
+if [[ ! -d $TMP_DIR ]] ; then
+    mkdir -p $TMP_DIR
+    StopIfError "Could not create TMP_DIR=$TMPDIR"
+    QuietAddExitTask cleanup_build_area_and_end_program
+fi
