88e7455ac7b1ed240c8898e7d7cf197fccb4723b
Gratien D'haese <gratien.dhaese@hp.com>
2014-04-02 15:43:54 +0200

    * rename section [remove] into [preremove] in the INI_FILE * fix the icapstatus file naming

--- opt/upgrade-ux/scripts/prep/hp/28_show_icapstatus.sh
+++ opt/upgrade-ux/scripts/prep/hp/28_show_icapstatus.sh
@@ -2,12 +2,12 @@
 # save the output of icapstatus before patching
 [[ ! -x /usr/sbin/icapstatus ]] && return
 
-[[ -f "$VAR_TMP/${HOSTNAME}_icapstatus_before.${DS}" ]] && return   # we already have the file
+[[ -f "$ICAPSTATUS_BEFORE" ]] && return   # we already have the file
 
 /usr/sbin/icapstatus -s 2>/dev/null > "$TMP_DIR/icapstatus_snapshot.txt"
 
 if [[ -s "$TMP_DIR/icapstatus_snapshot.txt" ]]; then
-    /usr/sbin/icapstatus > "$VAR_TMP/${HOSTNAME}_icapstatus_before.${DS}"
+    /usr/sbin/icapstatus > "$ICAPSTATUS_BEFORE"
     # save the snapshot info as well
-    cp "$TMP_DIR/icapstatus_snapshot.txt" "$VAR_TMP/${HOSTNAME}_icapstatus_snapshot.${DS}"
+    cp "$TMP_DIR/icapstatus_snapshot.txt" "$VAR_DIR/${HOSTNAME}_icapstatus_snapshot.${DS}"
 fi
