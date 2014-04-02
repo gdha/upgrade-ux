1c9511e03f1d16f22e3407bed72f61ee628c2422
Gratien D'haese <gratien.dhaese@hp.com>
2014-04-02 13:14:25 +0200

    improved the SG node status and report it correctly

--- opt/upgrade-ux/scripts/prep/hp/45_check_serviceguard.sh
+++ opt/upgrade-ux/scripts/prep/hp/45_check_serviceguard.sh
@@ -3,21 +3,32 @@
 
 [[ ! -x $CMVIEWCL ]] && return     # not a cluster
 
-if [[ ! -f $VAR_DIR/cmviewcl.txt ]]; then
-    $CMVIEWCL -v 2>/dev/null > $VAR_DIR/cmviewcl.txt
-fi
+$CMVIEWCL -v 2>/dev/null > $VAR_DIR/cmviewcl.txt
 
 CLNAME=$( head -3 $VAR_DIR/cmviewcl.txt | tail -1 | awk '{print $1}' )
+CLstatus=$( head -3 $VAR_DIR/cmviewcl.txt | tail -1 | awk '{print $2}' )
 SGversion=$( $SWLIST | grep -E '(T1905BA|T1905CA|B3935DA)' | awk '{print $2}' )
+NodeStatus=$( $CMVIEWCL -f line -l node | grep status | cut -d= -f2 )
+
 # what is the highest SG patch installed
 if [[ -f $VAR_DIR/${HOSTNAME}_show_patches.before ]]; then
     SGpatch=$( grep -i serviceguard $VAR_DIR/${HOSTNAME}_show_patches.before | grep PHSS | tail -1 | awk '{print $1}' )
 else
     SGpatch=$( $SWLIST -l patch | grep -i serviceguard | grep PHSS | tail -1 | awk '{print $1}' | cut -d. -f1 )
 fi
+
 SGminnr=${SGversion#A.11.}              # A.11.16.00 => 16.00
 SGminnr=${SGminnr%.00}                  # 16.00 => 16
 SGpatchnr=${SGpatch#PHSS_}              # PHSS_43094 => 43094
 
-LogPrint "Cluster $CLNAME (version $SGversion - latest patch $SGpatch) is UP"
 
+Log "Cluster $CLNAME (version $SGversion - latest patch $SGpatch) is ${CLstatus}"
+
+if [[ "${NodeStatus}" = "up" ]]; then
+    if (( PREVIEW_MODE )) ; then
+        Log "The node ($HOSTNAME) cluster status (${NodeStatus}) should be \"down\" when running $PRODUCT"
+    else
+        LogPrint "The node ($HOSTNAME) cluster status (${NodeStatus}) should be \"down\" when running $PRODUCT"
+	EnterNextStageAllowed="N"
+    fi
+fi
