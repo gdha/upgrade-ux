5bd7827ad879b23a8d97cf2391ed4caa865f4d8a
Gratien D'haese <gratien.dhaese@gmail.com>
2014-04-04 19:02:01 +0200

    fix the mktemp on Linux (fedora)

--- opt/upgrade-ux/scripts/prep/default/03_prepare_tmp_build_area.sh
+++ opt/upgrade-ux/scripts/prep/default/03_prepare_tmp_build_area.sh
@@ -2,13 +2,13 @@
 # 
 # create temporary work area and register removal exit task
 BUILD_DIR="$(mktemp)"
-mkdir -p $BUILD_DIR
+[[ ! -d $BUILD_DIR ]] && mkdir -p $BUILD_DIR
 StopIfError "Could not create build area '$BUILD_DIR'"
 QuietAddExitTask cleanup_build_area_and_end_program
 
 TMP_DIR=$BUILD_DIR
 if [[ ! -d $TMP_DIR ]] ; then
     mkdir -p $TMP_DIR
-    StopIfError "Could not create TMP_DIR=$TMPDIR"
+    StopIfError "Could not create TMP_DIR=$TMP_DIR"
     QuietAddExitTask cleanup_build_area_and_end_program
 fi
