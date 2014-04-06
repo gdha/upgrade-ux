96941c586b9bdf420e422dd720fa73bf7d72f2f8
Gratien D'haese <gratien.dhaese@hp.com>
2014-04-06 21:27:03 +0200

    introducing the init stage so we can work around other stages (later on)

--- opt/upgrade-ux/lib/upgrade-workflow.sh
+++ opt/upgrade-ux/lib/upgrade-workflow.sh
@@ -4,7 +4,9 @@ WORKFLOW_upgrade_DESCRIPTION="upgrade the operating system"
 set -A WORKFLOWS ${WORKFLOWS[@]} "upgrade"
 
 function WORKFLOW_upgrade {
+    SourceStage "init"
     SourceStage "prep"
     SourceStage "preremove"
+    SourceStage "preinstall"
     SourceStage "cleanup"
 }
---
+++ opt/upgrade-ux/scripts/init/default/10_check_current_status.sh
@@ -0,0 +1,11 @@
+# 10_check_current_status.sh
+# purpose: read the content of file $STATUS_FILE which contain flags where we are in the upgrade process
+# -------  as we may reboot and must be able to restart at the point where we left off
+#          The status will be appending to the $STATUS_FILE. 
+# the status of the last stage will be stored in variable CURRENT_STATUS
+# at each phase in the process we will change the value of CURRENT_STATUS ($stage:[start|ended])
+# flag looks like e.g.: 2014-03-20 13:23:59 init:ended
+
+CURRENT_STATUS=$( ReadCurrentStatus "$STATUS_FILE" )
+LogPrint "Current status is $CURRENT_STATUS"
+
