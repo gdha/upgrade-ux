ee6528cec3433244c5cc60283fcaf25593b2a848
Gratien D'haese <gratien.dhaese@hp.com>
2014-04-10 16:23:56 +0200

    Added _before for the file so we can compare later with the _after file

--- opt/upgrade-ux/scripts/prep/hp/29_check_global_patch_bundles.sh
+++ opt/upgrade-ux/scripts/prep/hp/29_check_global_patch_bundles.sh
@@ -2,21 +2,23 @@
 # this is a typically script for one customer only
 # check the global patch bundles already installed and verify if the one of last year is installed
 
-$SWLIST | grep gpb > "$TMP_DIR/${HOSTNAME}_global_patch_bundles.${DS}"
-if [[ -s "$TMP_DIR/${HOSTNAME}_global_patch_bundles.${DS}" ]]; then
-    mv "$TMP_DIR/${HOSTNAME}_global_patch_bundles.${DS}" "$VAR_DIR/${HOSTNAME}_global_patch_bundles.${DS}"
-    Log "Saved overview of all installed global patch bundles as $VAR_DIR/${HOSTNAME}_global_patch_bundles.${DS}"
+$SWLIST | grep gpb > "$TMP_DIR/${HOSTNAME}_global_patch_bundles_before.${DS}"
+if [[ -s "$TMP_DIR/${HOSTNAME}_global_patch_bundles_before.${DS}" ]]; then
+    mv "$TMP_DIR/${HOSTNAME}_global_patch_bundles_before.${DS}" "$VAR_DIR/${HOSTNAME}_global_patch_bundles_before.${DS}"
+    Log "Saved overview of all installed global patch bundles as $VAR_DIR/${HOSTNAME}_global_patch_bundles_before.${DS}"
 fi
 
-if [[ -f "$VAR_DIR/${HOSTNAME}_global_patch_bundles.${DS}" ]]; then
+if [[ -f "$VAR_DIR/${HOSTNAME}_global_patch_bundles_before.${DS}" ]]; then
     ThisYear=$( date +%Y )
     LastYear=$(( ThisYear - 1 ))
-    grep -q $LastYear "$VAR_DIR/${HOSTNAME}_global_patch_bundles.${DS}"
+    grep -q $LastYear "$VAR_DIR/${HOSTNAME}_global_patch_bundles_before.${DS}"
     if (( $? > 0 )) ; then
 	if (( PREVIEW_MODE )) ; then
             LogPrint  "WARNING: I am afraid the global patch bundle of last year ($LastYear) was not yet installed"
 	else
-            Error "I am afraid the global patch bundle of last year ($LastYear) was not yet installed"
+            #Error "I am afraid the global patch bundle of last year ($LastYear) was not yet installed"
+	    # if we are not that fast to install all patches in the same year...
+            LogPrint "ERROR: I am afraid the global patch bundle of last year ($LastYear) was not yet installed"
 	fi
     fi
 fi
