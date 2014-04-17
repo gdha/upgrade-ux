61bb8dfdc983d0ad07fe0101a216fd08a0c1e5ee
Gratien D'haese <gratien.dhaese@hp.com>
2014-04-17 16:47:46 +0200

    adding cleanup command and run commands defined in INI section

---
+++ opt/upgrade-ux/scripts/preinstall/hp/50_run_cleanup.sh
@@ -0,0 +1,9 @@
+# 50_run_cleanup.sh
+if (( PREVIEW )) ; then
+    Log "Run cleanup process [preview]"
+    [[ -x /usr/sbin/cleanup ]] && /usr/sbin/cleanup -p -c 2
+else
+    LogPrint "Run cleanup process"
+    [[ -x /usr/sbin/cleanup ]] && /usr/sbin/cleanup -c 2
+fi
+
