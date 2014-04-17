1346a9b13e9b333b806af9207fab97e4f15570ff
Gratien D'haese <gratien.dhaese@hp.com>
2014-04-17 10:08:58 +0200

    22_check_vrtsvcs.sh check veritas cluster software version. When <5.0 then EOL reached and warn or bail out with an error

---
+++ opt/upgrade-ux/scripts/prep/hp/22_check_vrtsvcs.sh
@@ -0,0 +1,16 @@
+# 22_check_vrtsvcs.sh
+# Veritas Cluster Server software versions < 5.0 are no longer supported and we
+# then advise not to patch anymore to avoid bringing the old CVS in danger
+
+grep -q VRTSvcs "$VAR_DIR/${HOSTNAME}_swlist_before.${DS}" || return  # no VCS installed
+
+$SWLIST VRTSvcs,r\>=5.0 >/dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+    LogPrint "Veritas Cluster Server software has reached EOL and is not supported anymore"
+    if (( PREVIEW_MODE )) ; then
+	LogPrint "We strongly discourage running $PRODUCT on this system"
+    else
+        Error "Due to EOL reached patching is not allowed anymore"
+    fi
+fi
+
