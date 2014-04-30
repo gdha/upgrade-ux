7d0fedf19d341920395cfdc33f5a08efd1115e74
Gratien D'haese <gratien.dhaese@hp.com>
2014-04-30 17:11:53 +0200

    finish stape monitoring disable script for HP-UX 11.11 and 11.23

--- opt/upgrade-ux/scripts/postinstall/hp/37_verify_stape_monitor.sh
+++ opt/upgrade-ux/scripts/postinstall/hp/37_verify_stape_monitor.sh
@@ -7,4 +7,34 @@ ps -ef | grep stape_monitor | grep -v grep > $TMP_DIR/stape_monitor
 [[ ! -s $TMP_DIR/stape_monitor ]] && return  # not running which is GOOD
 
 # steps to disable montoring for stape
-# we cannot script this (for the moment)
+[[ ! -x /etc/opt/resmon/lbin/monconfig ]] && return
+
+Log "Disable monitoring"
+/etc/opt/resmon/lbin/monconfig <<EOF
+K
+Y
+Q
+EOF
+
+Log "Disable stape monitoring"
+if [[ -f /usr/sbin/stm/uut/bin/tools/monitor/dm_stape ]]; then
+    [[ ! -d /usr/sbin/stm/uut/bin/tools/monitor/org ]] && mkdir /usr/sbin/stm/uut/bin/tools/monitor/org
+    mv /usr/sbin/stm/uut/bin/tools/monitor/dm_stape /usr/sbin/stm/uut/bin/tools/monitor/org
+fi
+
+if [[ -f /etc/opt/resmon/dictionary/dm_stape.dict ]]; then
+    [[ ! -d /etc/opt/resmon/dictionary/org ]] && mkdir /etc/opt/resmon/dictionary/org
+    mv /etc/opt/resmon/dictionary/dm_stape.dict /etc/opt/resmon/dictionary/org
+fi
+
+if [[ -f /var/stm/data/tools/monitor/dm_stape.hwa ]]; then
+    [[ ! -d /var/stm/data/tools/monitor/org ]] && mkdir /var/stm/data/tools/monitor/org
+    mv /var/stm/data/tools/monitor/dm_stape.hwa /var/stm/data/tools/monitor/org
+fi
+
+Log "Enable monitoring"
+/etc/opt/resmon/lbin/monconfig <<EOF
+E
+Q
+EOF
+
