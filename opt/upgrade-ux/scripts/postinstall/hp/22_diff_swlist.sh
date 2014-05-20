c47446e47336a7431c763e729621aa36d3f421f9
Gratien D'haese <gratien.dhaese@hp.com>
2014-05-20 16:34:00 +0200

    add verification after the upgrade of the swlist before and after

---
+++ opt/upgrade-ux/scripts/postinstall/hp/22_diff_swlist.sh
@@ -0,0 +1,15 @@
+# 22_diff_swlist.sh
+# compare the swlist.before with the swlist.after
+[[ ! -f "$VAR_DIR/$DS/${HOSTNAME}_swlist.before" ]]  &&  return
+[[ ! -f "$VAR_DIR/$DS/${HOSTNAME}_swlist.after"  ]]  &&  return
+
+if (( PREVIEW )) ; then
+    Log "Comparing software bundles [not in preview mode]"
+else
+    cmp -s "$VAR_DIR/$DS/${HOSTNAME}_swlist.before" "$VAR_DIR/$DS/${HOSTNAME}_swlist.after"
+    if [[ $? -eq 1 ]]; then
+        LogPrint "The following software bundles were modified after the upgrade:"
+	sdiff -s "$VAR_DIR/$DS/${HOSTNAME}_swlist.before" "$VAR_DIR/$DS/${HOSTNAME}_swlist.after"
+	sdiff -s "$VAR_DIR/$DS/${HOSTNAME}_swlist.before" "$VAR_DIR/$DS/${HOSTNAME}_swlist.after" >&2
+    fi
+fi
