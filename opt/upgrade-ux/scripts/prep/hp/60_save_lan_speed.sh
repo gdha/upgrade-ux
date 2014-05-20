c81e74cab11861c23334a6c04929d4f70c768a29
Gratien D'haese <gratien.dhaese@hp.com>
2014-05-20 10:16:00 +0200

    * version nr incremented to 0.2 in upgrade-ux & upgrade-ux.psf * 60_save_lan_speed.sh: fixed type in before and changed .txt into .before * upgrade-ux.8.txt got deleted again - why??

--- opt/upgrade-ux/scripts/prep/hp/60_save_lan_speed.sh
+++ opt/upgrade-ux/scripts/prep/hp/60_save_lan_speed.sh
@@ -1,6 +1,6 @@
 # 60_save_lan_speed.sh
 
-[[ -f "$VAR_TMP/$DS/lan_interfaces_speed.bofore" ]]  &&  return  # we already have the file
+[[ -f "$VAR_TMP/$DS/lan_interfaces_speed.before" ]]  &&  return  # we already have the file
 
 (
 
@@ -11,4 +11,4 @@ do
     echo " "
 done
 
-) > "$VAR_DIR/$DS/lan_interfaces_speed.txt"
+) > "$VAR_DIR/$DS/lan_interfaces_speed.before"
