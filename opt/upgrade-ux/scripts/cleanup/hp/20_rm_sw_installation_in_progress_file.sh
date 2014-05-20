5478e90f4f4a8ea13d93e8d392e10ca4309d9638
Gratien D'haese <gratien.dhaese@hp.com>
2014-05-20 15:50:41 +0200

    added new file cleanup/hp/20_rm_sw_installation_in_progress_file.sh : rename the file with suffix .preview

---
+++ opt/upgrade-ux/scripts/cleanup/hp/20_rm_sw_installation_in_progress_file.sh
@@ -0,0 +1,12 @@
+# 20_rm_sw_installation_in_progress_file.sh
+# only to be run in preview mode - rename the $VAR_DIR/$DS/sw_installation_in_progress file
+# otherwise we might have the wrong count during sw installation - see issue #3
+
+if (( PREVIEW )) ; then
+
+    if [[ -f "$VAR_DIR/$DS/sw_installation_in_progress" ]]; then
+        mv "$VAR_DIR/$DS/sw_installation_in_progress" "$VAR_DIR/$DS/sw_installation_in_progress.preview"
+	Log "Saved the history of software installation in $VAR_DIR/$DS/sw_installation_in_progress.preview"
+    fi
+
+fi
