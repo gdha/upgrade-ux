c4decf7cab92b9093cf8e7ec4058b094a72eb640
Gratien D'haese <gratien.dhaese@hp.com>
2014-05-21 15:32:53 +0200

    the status file should be appended not overwritten (by 90_preview_ended_successfully.sh)

--- opt/upgrade-ux/scripts/cleanup/default/90_preview_ended_successfully.sh
+++ opt/upgrade-ux/scripts/cleanup/default/90_preview_ended_successfully.sh
@@ -7,8 +7,8 @@ if (( PREVIEW )) ; then
     LogPrint "Preview mode ended successfully"
     echo "$(date)  (preview ended successfully)" > "$PREVIEW_FILE"
     # next line will be read by init/default/15_check_preview_run.sh to decide for upgrade mode
-    echo "$(Stamp)preview:ended   (preview ended successfully)" > "$STATUS_FILE"
+    echo "$(Stamp)preview:ended   (preview ended successfully)" >> "$STATUS_FILE"
 else
     LogPrint "Upgrade mode ended successfully"
-    echo "$(Stamp)upgrade:ended   (upgrade ended successfully)" > "$STATUS_FILE"
+    echo "$(Stamp)upgrade:ended   (upgrade ended successfully)" >> "$STATUS_FILE"
 fi
