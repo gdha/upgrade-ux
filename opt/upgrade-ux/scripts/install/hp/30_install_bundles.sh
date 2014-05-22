42d7bd05197e015af7d4356f02db93a612542c8e
Gratien D'haese <gratien.dhaese@hp.com>
2014-05-22 15:44:30 +0200

    30_install_bundles.sh - add " arround the filename

--- opt/upgrade-ux/scripts/install/hp/30_install_bundles.sh
+++ opt/upgrade-ux/scripts/install/hp/30_install_bundles.sh
@@ -24,17 +24,17 @@ do
 
     if (( PREVIEW )) ; then
         LogPrint "${command[i]} -p -v ${options[i]} ${source[i]} $my_bundle"
-	printf "${command[i]} -p -v ${options[i]} ${source[i]} $my_bundle" >> $VAR_DIR/$DS/sw_installation_in_progress
+	printf "${command[i]} -p -v ${options[i]} ${source[i]} $my_bundle" >> "$VAR_DIR/$DS/sw_installation_in_progress"
 	${command[i]} -p -v ${options[i]} ${source[i]} "$my_bundle"
 	rc=$?
     else
         LogPrint "${command[i]} -v ${options[i]} ${source[i]} $my_bundle"
-	printf "${command[i]} -v ${options[i]} ${source[i]} $my_bundle" >> $VAR_DIR/$DS/sw_installation_in_progress
+	printf "${command[i]} -v ${options[i]} ${source[i]} $my_bundle" >> "$VAR_DIR/$DS/sw_installation_in_progress"
         ${command[i]} -v ${options[i]} ${source[i]} "$my_bundle"
 	rc=$?
     fi
 
-    printf " rc=$rc\n" >> $VAR_DIR/$DS/sw_installation_in_progress
+    printf " rc=$rc\n" >> "$VAR_DIR/$DS/sw_installation_in_progress"
     if [[ $rc -gt 0 ]]; then
         LogPrint "Error detected: ${command[i]} -v ${options[i]} ${source[i]} $my_bundle"
     fi
@@ -43,7 +43,7 @@ do
     if [[ "${command[i]}" = "$SWINSTALL" ]]; then
 	SwJob ${command[i]} >&2
     elif [[ "${command[i]}" = "/usr/sbin/update-ux" ]]; then
-	TODAY=$(date | awk '{print $1, $2, $3}')
+	#TODAY=$(date | awk '{print $1, $2, $3}')
         #echo $(SurroundingGrep 0 1000 "$TODAY" /var/adm/sw/update-ux.log) >&2
         tail -20 /var/opt/swm/swm.log >&2
     fi
