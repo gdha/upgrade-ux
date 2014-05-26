ccfa948bbcee22a4e9a7b35a87125f6ef5302874
Gratien D'haese <gratien.dhaese@hp.com>
2014-05-26 15:27:49 +0200

    pre-define daysup=0 in preinstall/default/05_reboot_required.sh - related to issue #7

--- opt/upgrade-ux/scripts/preinstall/default/05_reboot_required.sh
+++ opt/upgrade-ux/scripts/preinstall/default/05_reboot_required.sh
@@ -1,6 +1,9 @@
 # 5_reboot_required.sh
 # if the uptime of the system is higher then 30 days then we propose to reboot first
 
+# pre-define daysup as 0 (we can check this variable later [after a reboot])
+daysup=0
+
 # when uptime is not expressed in days then we probably just rebooted our system
 uptime | grep -q days || return
 
