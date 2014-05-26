5b05bd6317b7f79a7c39de63332f7ed0306a0ed5
Gratien D'haese <gratien.dhaese@hp.com>
2014-05-26 11:17:18 +0200

    * added man page (and documentation to come) to the PSF file * create/remove the man page /usr/share/man/man8.Z/upgrade-ux.8

--- packaging/HP-UX/postinstall.sh
+++ packaging/HP-UX/postinstall.sh
@@ -4,4 +4,9 @@
 echo "       * upgrade-ux postinstall.sh script runs"
 
 # man page (copy a compressed file to /usr/share/man/man8.Z/upgrade-ux.8)
-
+[[ -f /usr/share/man/man8.Z/upgrade-ux.8 ]] && rm -f /usr/share/man/man8.Z/upgrade-ux.8
+if [[ -f /opt/upgrade-ux/man/upgrade-ux.8 ]]; then
+    compress -c /opt/upgrade-ux/man/upgrade-ux.8 > /usr/share/man/man8.Z/upgrade-ux.8
+    chmod 644 /usr/share/man/man8.Z/upgrade-ux.8
+    echo "       * Create man page under /usr/share/man/man8.Z/upgrade-ux.8"
+fi
--- packaging/HP-UX/postremove.sh
+++ packaging/HP-UX/postremove.sh
@@ -4,4 +4,5 @@
 echo "       * postremove.sh starts"
 
 # remove man page
-
+[[ -f /usr/share/man/man8.Z/upgrade-ux.8 ]] && rm -f /usr/share/man/man8.Z/upgrade-ux.8
+echo "       * Removed /usr/share/man/man8.Z/upgrade-ux.8 man page"
