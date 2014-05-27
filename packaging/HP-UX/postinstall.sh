7f20cc684672ed3116c03ff0cb949640517dd6a6
Gratien D'haese <gratien.dhaese@hp.com>
2014-05-27 14:19:35 +0200

    man page was double compressed (by Makefile and by postinstall.sh script) - once is enough ;-)

--- packaging/HP-UX/postinstall.sh
+++ packaging/HP-UX/postinstall.sh
@@ -6,7 +6,8 @@ echo "       * upgrade-ux postinstall.sh script runs"
 # man page (copy a compressed file to /usr/share/man/man8.Z/upgrade-ux.8)
 [[ -f /usr/share/man/man8.Z/upgrade-ux.8 ]] && rm -f /usr/share/man/man8.Z/upgrade-ux.8
 if [[ -f /opt/upgrade-ux/man/upgrade-ux.8 ]]; then
-    compress -c /opt/upgrade-ux/man/upgrade-ux.8 > /usr/share/man/man8.Z/upgrade-ux.8
+    # /opt/upgrade-ux/man/upgrade-ux.8 already compressed by Makefile (of packaging)
+    cp /opt/upgrade-ux/man/upgrade-ux.8  /usr/share/man/man8.Z/upgrade-ux.8
     chmod 644 /usr/share/man/man8.Z/upgrade-ux.8
     echo "       * Create man page under /usr/share/man/man8.Z/upgrade-ux.8"
 fi
