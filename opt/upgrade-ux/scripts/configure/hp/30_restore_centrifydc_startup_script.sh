345945d0b269a27bedec6101238b835ccfd02061
Gratien D'haese <gratien.dhaese@hp.com>
2014-05-27 11:34:30 +0200

    modified configure/hp/30_restore_centrifydc_startup_script.sh to restore /sbin/init.d/centrifydc file in case a new version of centrifyDC was installed

--- opt/upgrade-ux/scripts/configure/hp/30_restore_centrifydc_startup_script.sh
+++ opt/upgrade-ux/scripts/configure/hp/30_restore_centrifydc_startup_script.sh
@@ -2,17 +2,16 @@
 
 [[ ! -x /usr/bin/adinfo ]] && return     # adinfo is not available when CentrifyDC is not installed
 
-[[ ! -f "$VAR_DIR/$DS/centrifydc.version.before" ]]  &&  return  # cannot cmp so return
-[[ ! -f "$VAR_DIR/$DS/centrifydc.version.after" ]]  &&  return  # cannot cmp so return
+# ls -l centrifydc-adinfo*.after
+# -rw-r--r--   1 root       sys            272 May 23 09:41 centrifydc-adinfo-3.0.5-506.after
+# -rw-------   1 root       sys            372 May 23 10:33 centrifydc-adinfo-5.1.1-831.after
 
-cmp -s "$VAR_DIR/$DS/centrifydc.version.before"  "$VAR_DIR/$DS/centrifydc.version.after"
-if [[ $? -eq 1 ]]; then
+if (( $(ls -l $VAR_DIR/$DS/centrifydc-adinfo*.after | wc -l) > 1 )) ; then
     # centrifyDC has been updated; so make extra copy of /sbin/init.d/centrifydc
-    cp /sbin/init.d/centrifydc  "/sbin/init.d/centrifydc.$(cat $VAR_DIR/$DS/centrifydc.version.after)"
     if (( PREVIEW )); then
 	Log "Restored the original /sbin/init.d/centrifydc file [not in preview mode]"
     else
-        cp "$VAR_DIR/$DS/centrifydc.before" /sbin/init.d/centrifydc
+        cp -p "$VAR_DIR/$DS/centrifydc.before" /sbin/init.d/centrifydc
         chmod 755 /sbin/init.d/centrifydc
         Log "Restored the original /sbin/init.d/centrifydc file"
     fi
