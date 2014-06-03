ba7672c1e735c01f752fd995e37e1a007ff4984d
Gratien D'haese <gratien.dhaese@hp.com>
2014-06-03 13:03:46 +0200

    postinstall/hp/27_show_cimprovider.sh: check if cimserver is running; if not start it close issue #9

--- opt/upgrade-ux/scripts/postinstall/hp/27_show_cimprovider.sh
+++ opt/upgrade-ux/scripts/postinstall/hp/27_show_cimprovider.sh
@@ -2,6 +2,14 @@
 # show all cimproviders which do not have the OK flag (informative)
 [[ ! -x /opt/wbem/bin/cimprovider ]] && return  # if not there we do not care (in this stage)
 
+# need to check if cimserver is running - see issue #9
+ps -ef | grep cimservermain | grep -vq grep
+if [[ $? -eq 1 ]] ; then
+    # start cimserver
+    Log "Note: cimserver was not running - starting it now"
+    [[ -x /opt/wbem/sbin/cimserver ]] && /opt/wbem/sbin/cimserver  >&2
+fi
+
 /opt/wbem/bin/cimprovider -ls > "$VAR_DIR/$DS/${HOSTNAME}_cimprovider.after"
 
 grep -v "^MODULE" "$VAR_DIR/$DS/${HOSTNAME}_cimprovider.after" | grep -vq "OK"
