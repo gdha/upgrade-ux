10cc1b0fbb9fa4249dc9f956f2c742b2ba766b33
Gratien D'haese <gratien.dhaese@hp.com>
2014-06-17 14:30:49 +0200

    implemented the validate recommendations (of Linux) on cleanup scripts

--- opt/upgrade-ux/scripts/cleanup/hp/45_grep_error_in_logfile.sh
+++ opt/upgrade-ux/scripts/cleanup/hp/45_grep_error_in_logfile.sh
@@ -1,5 +1,5 @@
 # 45_grep_error_in_logfile.sh
 grep -q "ERROR:" "$LOGFILE"
-if [[ $? -eq 0 ]]; then
+if (( $? == 0 )); then
     LogPrint "!!!!! ERRORS found in the logfile - please verify !!!!!"
 fi
--- opt/upgrade-ux/scripts/cleanup/hp/46_verify_insufficient_space.sh
+++ opt/upgrade-ux/scripts/cleanup/hp/46_verify_insufficient_space.sh
@@ -1,5 +1,5 @@
 # 46_verify_insufficient_space.sh
 grep -q "insufficient" "$LOGFILE"
-if [[ $? -eq 0 ]]; then
+if (( $? == 0 )); then
     LogPrint "$( SurroundingGrep 0 4 'insufficient' $LOGFILE )"
 fi
