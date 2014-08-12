62cc088214bcbc65067e692984d8a81af5081e33
Gratien D'haese <gratien.dhaese@gmail.com>
2014-08-12 10:56:53 +0200

    improve the ipconf script

--- opt/upgrade-ux/scripts/prep/hp/66_save_ipfconf.sh
+++ opt/upgrade-ux/scripts/prep/hp/66_save_ipfconf.sh
@@ -1,26 +1,29 @@
 # 66_save_ipfconf.sh
 
-[[ ! -f /etc/rc.config.d/ipfconf ]] && return   # bundle B9901AA (IPF) not installed
+[[ ! -f /etc/rc.config.d/ipfconf ]] && return   # bundle B9901AA (or IPFilter) not installed
 
 # save the original startup configuration file
 cp -p /etc/rc.config.d/ipfconf   "$VAR_DIR/$DS/ipfconf.before"
 
-# check if before patching ipf is enabled:
+# check "before" patching if ipfilter was already enabled:
 if (( $(ch_rc -l -p IPF_START /etc/rc.config.d/ipfconf) )); then
-    # IPF is enabled
+    # IPF is enabled (must be done by user on purpose)
     LogPrint "WARNING: IP Filtering is enabled on system $lhost"
 else
     LogPrint "IP Filtering is disabled on system $lhost"
-    # make a mark of this fact as after patching is gets enabled by default on HP-UX 11.23
-    # we must turn it back off to avoid SG panics
+    # make a script as "after" patching IPFilter gets enabled by default on HP-UX 11.23
+    # we must turn it back off to avoid SG problems
     echo "/sbin/init.d/ipfboot stop"                            > "$VAR_DIR/$DS/ipf-fix.script"
     echo "ch_rc -a -p IPF_START=0 /etc/rc.config.d/ipfconf"    >> "$VAR_DIR/$DS/ipf-fix.script"
     echo "ch_rc -a -p IPMON_START=0 /etc/rc.config.d/ipfconf"  >> "$VAR_DIR/$DS/ipf-fix.script"
+    echo "[[ -s /etc/opt/ipf/ipf.conf ]] && cp -p /etc/opt/ipf/ipf.conf /etc/opt/ipf/ipf.conf.\$(date '+%Y%m%d')" >> "$VAR_DIR/$DS/i
+pf-fix.script"
+    echo "[[ -s /etc/opt/ipf/ipf.conf ]] && > /etc/opt/ipf/ipf.conf" >> "$VAR_DIR/$DS/ipf-fix.script"
     chmod 755 "$VAR_DIR/$DS/ipf-fix.script"
     LogPrint "Created IPF fix script to be executed automatically after patching"
 fi
 
 # check the current activity with ipfstat and save this also
-[[ ! -x /sbin/ipfstat ]] && return  # cannot save output
+[[ -x /sbin/ipfstat ]] && /sbin/ipfstat > "$VAR_DIR/$DS/ipfstat.before"                # HP-UX 11.23
+[[ -x /opt/ipf/bin/ipfstat ]] && /opt/ipf/bin/ipfstat > "$VAR_DIR/$DS/ipfstat.before"  # HP-UX 11.31
 
-/sbin/ipfstat > "$VAR_DIR/$DS/ipfstat.before"
