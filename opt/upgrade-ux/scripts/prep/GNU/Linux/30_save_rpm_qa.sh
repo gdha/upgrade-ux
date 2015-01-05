d2e0bc49ed63460926ca79bb16b588d7f8d1cb79
Gratien D'haese <gratien.dhaese@gmail.com>
2015-01-05 14:59:19 +0100

    moving prep/fedora/30_save_rpm_qa.sh to prep/GNU/Linux/30_save_rpm_qa.sh and adding check on $RPM executable (makes more sense)

--- opt/upgrade-ux/scripts/prep/GNU/Linux/30_save_rpm_qa.sh
+++ opt/upgrade-ux/scripts/prep/GNU/Linux/30_save_rpm_qa.sh
@@ -1,3 +1,7 @@
+# prep/GNU/Linux/30_save_rpm_qa.sh
+
 [[ -f "$VAR_DIR/$DS/${HOSTNAME}_rpm_qa.before" ]] && return  # we have the output already
+
+[[ ! -x $RPM ]] && return  # rpm executable not available; just return
 
 $RPM -qa > "$VAR_DIR/$DS/${HOSTNAME}_rpm_qa.before"
