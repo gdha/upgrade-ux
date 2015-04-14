be5f1fe7a477c67c4ace0d3cb4be73ee40a43ea9
Gratien D'haese <gratien.dhaese@hp.com>
2015-04-14 13:41:55 +0200

    added the mount output scripts (prep and postinstall) close #34

---
+++ opt/upgrade-ux/scripts/postinstall/default/35_save_mount_output.sh
@@ -0,0 +1,15 @@
+# 35_save_mount_output.sh
+# after patching we save another mount output as reference to proof patching was not to blaim
+# for certain not yet happened issues (you would be surprised what people blame patching for...)
+
+mount > "$VAR_DIR/$DS/mount_output.after"
+
+if [[ -f "$VAR_DIR/$DS/mount_output.before" ]]; then
+    cmp -s "$VAR_DIR/$DS/mount_output.before" "$VAR_DIR/$DS/mount_output.after"
+    if (( $? == 1 )) ; then
+        LogPrint "The mount output is different after patching (see log)"
+	sdiff -s "$VAR_DIR/$DS/mount_output.before" "$VAR_DIR/$DS/mount_output.after" >&2
+    else
+        Log "The mount output after patching is the same as before [ok]"
+    fi
+fi
---
+++ opt/upgrade-ux/scripts/prep/default/35_save_mount_output.sh
@@ -0,0 +1,3 @@
+# 35_save_mount_output.sh
+
+mount > "$VAR_DIR/$DS/mount_output.before"
