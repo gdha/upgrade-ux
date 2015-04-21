b9eb443e27facb5d32534d625e96f54abebb654d
Gratien D'haese <gratien.dhaese@gmail.com>
2015-04-21 17:13:57 +0200

    new script configure/hp/80_verify_init_level.sh - see issue #37

---
+++ opt/upgrade-ux/scripts/configure/hp/80_verify_init_level.sh
@@ -0,0 +1,7 @@
+# 80_verify_init_level.sh
+# The init level is sometimes manually edited to level 2 and after succesful patching set to 3 again
+# However, sometimes it is forgotten
+i=$( grep ^init /etc/inittab | cut -d: -f2 )  # should be 3, but could be 2
+if (( i < 3 )) ; then
+    LogPrint "Do not forget to change the init level to 3 in /etc/inittab"
+fi
