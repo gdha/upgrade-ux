2e24f19a5bac3a3d13a3bf275384b5f424e4977e
Gratien D'haese <gratien.dhaese@hp.com>
2015-09-28 16:58:39 +0200

    new script 70_check_openssl_logjam.sh to verify httpd config files on vulnerability CVE-2015-4000 issue #46

---
+++ opt/upgrade-ux/scripts/prep/default/70_check_openssl_logjam.sh
@@ -0,0 +1,31 @@
+# 70_check_openssl_logjam.sh
+# Script should work on all kind of UNIX flavours
+
+# list up all httpd related config files (apache, tomcat, hp smh, ....)
+find /usr -name "http*.conf" | grep -vE '(man|grep)' >  $TMP_DIR/list_httpd_conf_files.txt
+find /opt -name "http*.conf" | grep -vE '(man|grep)' >> $TMP_DIR/list_httpd_conf_files.txt
+[[ -s $TMP_DIR/list_httpd_conf_files.txt ]] && return   # no httpd.conf found = not vulnerable
+
+# list up http conf files containing the keyword "SSLCipherSuite"
+for FILE in $(cat $TMP_DIR/list_httpd_conf_files.txt)
+do
+    [[ ! -f "$FILE" ]] && continue  # to be sure we are dealing with a file
+    grep -q "SSLCipherSuite" "$FILE" 
+    if [[ $? -eq 0 ]]; then
+        echo "$FILE" >> $TMP_DIR/list_httpd_conf_files_containing_SSLCipherSuite_keyword
+    fi
+done
+
+# if we find argument +EXP as an option with keyword SSLCipherSuite then we are vulnerable
+for FILE in $(cat $TMP_DIR/list_httpd_conf_files_containing_SSLCipherSuite_keyword)
+do
+    grep "SSLCipherSuite" "$FILE" | grep -v "^#SSLCipherSuite" | grep -q "+EXP"
+    if [[ $? -eq 0 ]]; then
+        LogPrint "File $FILE is vulnerable with the OpenSSL LogJam (CVE-2015-4000) issue" 
+	# we create a empty file CVE-2015-4000 as a mark-point during the configure phase
+	# to fix this
+	touch  "$VAR_DIR/$DS/CVE-2015-4000"
+    else
+        Log "File $FILE is \"not\" vulnerable with the OpenSSL LogJam (CVE-2015-4000)"
+    fi
+done
