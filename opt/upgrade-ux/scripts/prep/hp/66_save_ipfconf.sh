c3d0457a7573e61bf91df3657f2345bfe49c58ff
Gratien D'haese <gratien.dhaese@hp.com>
2016-02-18 14:47:12 +0100

    66_save_ipfconf.sh: replace lhost by HOSTNAME

--- opt/upgrade-ux/scripts/prep/hp/66_save_ipfconf.sh
+++ opt/upgrade-ux/scripts/prep/hp/66_save_ipfconf.sh
@@ -8,9 +8,9 @@ cp -p /etc/rc.config.d/ipfconf   "$VAR_DIR/$DS/ipfconf.before"
 # check "before" patching if ipfilter was already enabled:
 if (( $(ch_rc -l -p IPF_START /etc/rc.config.d/ipfconf) )); then
     # IPF is enabled (must be done by user on purpose)
-    LogPrint "WARNING: IP Filtering is enabled on system $lhost"
+    LogPrint "WARNING: IP Filtering is enabled on system $HOSTNAME"
 else
-    LogPrint "IP Filtering is disabled on system $lhost"
+    LogPrint "IP Filtering is disabled on system $HOSTNAME"
     # make a script as "after" patching IPFilter gets enabled by default on HP-UX 11.23
     # we must turn it back off to avoid SG problems
     echo "/sbin/init.d/ipfboot stop"                            > "$VAR_DIR/$DS/ipf-fix.script"
