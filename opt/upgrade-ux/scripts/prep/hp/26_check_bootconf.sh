7abf7cfaf0f7752f3c788ebe4919c159199cb3ac
Gratien D'haese <gratien.dhaese@hp.com>
2016-02-22 16:54:07 +0100

    prep/hp/26_check_bootconf.sh: rewrote the script a bit to have a better logic overview of the possible issues we see with /stand/bootconf. The previous script has some misleading messages See issue #50

--- opt/upgrade-ux/scripts/prep/hp/26_check_bootconf.sh
+++ opt/upgrade-ux/scripts/prep/hp/26_check_bootconf.sh
@@ -4,6 +4,17 @@
 a=$(setboot | grep Primary | awk '{ print $4 }' | sed -e 's#/#\\/#g')
 [[ -z "$a" ]] && Error "setboot does not have a Primary disk!"
 
+set -A BootableDisks $( /usr/sbin/lvlnboot -v 2>/dev/null | grep "Boot Disk" | awk '{print $1}' )
+
+if [[ ! -f /stand/bootconf ]]; then
+    LogPrint "ERROR: Critical file /stand/bootconf missing and should contain:"
+    for disk in $(echo ${BootableDisks[@]})
+    do
+        LogPrint "  l  $disk"
+    done
+    (( FORCED )) || EnterNextStageAllowed="N"
+fi
+
 case "$OS_VERSION" in
     11.11|11.23)
         grep $(ioscan -kfnC disk |  awk 'c&&!--c;/'$a'/{c=1}' | awk '{print $1}') /stand/bootconf > $TMP_DIR/bootdisk
@@ -18,10 +29,9 @@ case "$OS_VERSION" in
         ;;
 esac
 
-set -A BootableDisks $( /usr/sbin/lvlnboot -v 2>/dev/null | grep "Boot Disk" | awk '{print $1}' )
 
 if [[ ! -s $TMP_DIR/bootdisk ]]; then
-    LogPrint "ERROR: The /stand/bootconf file is missing and should contain"
+    LogPrint "ERROR: The /stand/bootconf file might be missing some entries:"
     for disk in $(echo ${BootableDisks[@]})
     do
         LogPrint "  l  $disk"
@@ -29,23 +39,20 @@ if [[ ! -s $TMP_DIR/bootdisk ]]; then
     (( FORCED )) || EnterNextStageAllowed="N"
 fi
 
-if [[ ! -f /stand/bootconf ]]; then
-    LogPrint "ERROR: Critical file /stand/bootconf missing"
-else
-    countbdsk=${#BootableDisks[@]}
-    countbcfg=$(wc -l /stand/bootconf 2>/dev/null | awk '{print $1}')
-    [[ -z "$countbcfg" ]] && countbcfg=0
+# investigate the boot disk(s) listed in /stand/bootconf
+countbdsk=${#BootableDisks[@]}
+countbcfg=$(wc -l /stand/bootconf 2>/dev/null | awk '{print $1}')
+[[ -z "$countbcfg" ]] && countbcfg=0
 
-    if (( countbdsk != countbcfg )) ; then
-        LogPrint "The primary bootpath does not match the entry in /stand/bootconf"
-        for disk in $(echo ${BootableDisks[@]})
-        do
-            grep -q $disk /stand/bootconf
-	    if (( $? > 0 )) ; then
-                LogPrint "ERROR: The /stand/bootconf file is missing the following line"
-	        LogPrint "  l  $disk"
-    	    fi
-        done
-        (( FORCED )) || EnterNextStageAllowed="N"
-    fi
+if (( countbdsk != countbcfg )) ; then
+    LogPrint "The primary bootpath does not match the entry in /stand/bootconf"
+    for disk in $(echo ${BootableDisks[@]})
+    do
+        grep -q $disk /stand/bootconf
+        if (( $? > 0 )) ; then
+            LogPrint "ERROR: The /stand/bootconf file is missing the following line"
+            LogPrint "  l  $disk"
+        fi
+    done
+    (( FORCED )) || EnterNextStageAllowed="N"
 fi
