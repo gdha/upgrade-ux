426c328d2f461f872b9cdc069755c463293715ae
Gratien D'haese <gratien.dhaese@gmail.com>
2016-08-16 13:19:06 +0200

    Add PREVIEW mode into script 45_disable_swat_in_inetd.sh

--- opt/upgrade-ux/scripts/configure/hp/45_disable_swat_in_inetd.sh
+++ opt/upgrade-ux/scripts/configure/hp/45_disable_swat_in_inetd.sh
@@ -5,11 +5,16 @@
 grep -v \# "$VAR_DIR/$DS/inetd_conf.after" | grep -q ^swat
 if (( $? == 0 )); then
     # we found an active entry; we should disable it. We will create a inetd_conf.new first
-    LogPrint "Disable the swat line in inetd.conf file"
+    Log "Disable the swat line in $VAR_DIR/$DS/inetd_conf.new file"
     sed -e 's/^swat/#swat/g' < "$VAR_DIR/$DS/inetd_conf.after"  > "$VAR_DIR/$DS/inetd_conf.new"
-    cp "$VAR_DIR/$DS/inetd_conf.new" /etc/inetd.conf
-    chown root:sys /etc/inetd.conf
-    chmod 644 /etc/inetd.conf
-    LogPrint "Inetd daemon re-reads its inetd.conf file now"
-    /usr/sbin/inetd -c
+    if (( PREVIEW )) ; then
+        Log "The \"swat\" entry in /etc/inetd.conf must be commented [not in preview mode]"
+    else
+        Log "Copy $VAR_DIR/$DS/inetd_conf.new to /etc/inetd.conf"
+        cp -p "$VAR_DIR/$DS/inetd_conf.new" /etc/inetd.conf
+        chown root:sys /etc/inetd.conf
+        chmod 644 /etc/inetd.conf
+        Log "Inetd daemon re-reads its inetd.conf file now"
+        /usr/sbin/inetd -c
+    fi
 fi
