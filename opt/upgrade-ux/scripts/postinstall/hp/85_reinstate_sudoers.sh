eb805f6f0f1b19a39666a099d45a94ce043d8e02
Gratien D'haese <gratien.dhaese@gmail.com>
2016-11-05 09:01:22 +0100

    To fix the problem with: 2016-11-04 14:15:29 Including postinstall/hp/85_reinstate_sudoers.sh 2016-11-04 14:15:30 Copy back the original /etc/sudoers file: /etc/sudoers: No such file or directory chmod: can't access /etc/sudoers
    We forgot the else clausule with the 'cmp' command.
    Issue #64

--- opt/upgrade-ux/scripts/postinstall/hp/85_reinstate_sudoers.sh
+++ opt/upgrade-ux/scripts/postinstall/hp/85_reinstate_sudoers.sh
@@ -16,20 +16,30 @@ SUDOVER=$( $SWLIST sudo 2>/dev/null | grep \# | tail -1 | awk '{print $3}' )
 if (( PREVIEW )) ; then
     Log "Copy back the original /etc/sudoers file [not in preview]"
 else
-    Log "Copy back the original /etc/sudoers file:"
+    Log "Move the original /etc/sudoers file to /etc/sudoers.$SUDOVER"
     mv -f /etc/sudoers /etc/sudoers.$SUDOVER         # move the fresh sudoers file
     # we have 2 copies "$VAR_DIR/$DS/sudoers.before" and "/etc/sudoers.$PRODUCT.before"
     # and these should be the same - if not only use "/etc/sudoers.$PRODUCT.before" to copy back
     if [[ -f "/etc/sudoers.${PRODUCT}.before" ]] ; then
         cmp -s "$VAR_DIR/$DS/sudoers.before" "/etc/sudoers.${PRODUCT}.before"
         if (( $? == 1 )); then
+            # if these files are not the same use the /etc/sudoers.${PRODUCT}.before one
             Log "Using /etc/sudoers.${PRODUCT}.before to copy back to /etc/sudoers"
             cp -p "/etc/sudoers.${PRODUCT}.before" /etc/sudoers
+        else
+            Log "Using $VAR_DIR/$DS/sudoers.before to copy back to /etc/sudoers"
+            cp -p "$VAR_DIR/$DS/sudoers.before" /etc/sudoers # re-instate the original sudoers
         fi
     else
+        # faillback in case /etc/sudoers.${PRODUCT}.before did not exist
         Log "Using $VAR_DIR/$DS/sudoers.before to copy back to /etc/sudoers"
         cp -p "$VAR_DIR/$DS/sudoers.before" /etc/sudoers # re-instate the original sudoers
     fi
     chown root:root /etc/sudoers
     chmod 440 /etc/sudoers
+fi
+
+# an extra step to verify the /etc/sudoers was not deleted by accident
+if [[ ! -f /etc/sudoers ]] ; then
+    LogPrint "ERROR: File /etc/sudoers does not exist!"
 fi
