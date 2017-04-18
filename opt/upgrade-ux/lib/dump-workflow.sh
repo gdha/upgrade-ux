e0614e2537bf8403a3c081388cf8751f6cb6cccb
Gratien D'haese <gratien.dhaese@gmail.com>
2017-04-18 17:18:50 +0200

    Make the INI file visible during the dump workflow - issue #67

--- opt/upgrade-ux/lib/dump-workflow.sh
+++ opt/upgrade-ux/lib/dump-workflow.sh
@@ -4,6 +4,7 @@ WORKFLOW_dump_DESCRIPTION="dump configuration and system information"
 set -A WORKFLOWS ${WORKFLOWS[@]} "dump"
 
 function WORKFLOW_dump  {
+
     LogPrint "Dumping out configuration and system information"
 
     if [ "$ARCH" != "$REAL_ARCH" ] ; then
@@ -33,4 +34,12 @@ function WORKFLOW_dump  {
                                 )" )"
     done
 
+    # see also script scripts/init/05_select_ini_file.sh
+    case "$OS" in
+        "HP-UX"|"SunOS") INI_FILE="${ETC_DIR}/${OS}-${OS_VERSION}-${YEAR}.ini" ;;
+        "GNU/Linux")     INI_FILE="${ETC_DIR}/${OS}-${OS_VENDOR}-${OS_VERSION}-${YEAR}.ini" ;;
+        *)               INI_FILE="${ETC_DIR}/${OS}-${OS_VENDOR}-${OS_VERSION}.ini" ;;
+    esac
+
+    LogPrint "$( printf "%40s : %s" "$(basename $INI_FILE)" "$( test -s "$INI_FILE" && echo OK || echo missing/empty )" )"
 }
