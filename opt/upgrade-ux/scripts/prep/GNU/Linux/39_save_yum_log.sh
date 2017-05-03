720375e8f667066752de0ff3a94f1dfef510eef9
Gratien D'haese <gratien.dhaese@gmail.com>
2017-05-03 16:37:28 +0200

    save the yum.log file

---
+++ opt/upgrade-ux/scripts/prep/GNU/Linux/39_save_yum_log.sh
@@ -0,0 +1,5 @@
+# 39_save_yum_log.sh
+# Keep a copy of the latest yum.log file
+
+[[ -f "$VAR_DIR/$DS/yum.log" ]] && return  # if we have the yum.log file then there is no need to copy it again
+cp  /var/log/yum.log "$VAR_DIR/$DS/yum.log"
