bc5df441b460c281293c1eb45e03df49f684c5e6
Gratien D'haese <gratien.dhaese@gmail.com>
2017-05-15 13:13:41 +0200

    Addeded the REQUIRED_PROGS array in configuration files and the prep script 95_check_missing_programs.sh

---
+++ opt/upgrade-ux/scripts/prep/default/95_check_missing_programs.sh
@@ -0,0 +1,18 @@
+# prep/default/95_check_missing_programs.sh
+# The main script usr/sbin/rear has the same kind of code
+# but other prep scripts can add binaries to the REQUIRED_PROGS array
+# so we need to double check before leaving the prep stage.
+
+# check for requirements, do we have all required binaries ?
+# define an empty array MISSING_PROGS
+set -A MISSING_PROGS
+for f in "${REQUIRED_PROGS[@]}" ; do
+        if ! has_binary "$f"; then
+                set -A MISSING_PROGS "${MISSING_PROGS[@]}" "$f"
+        fi
+done
+[[ -z "$MISSING_PROGS" ]]
+StopIfError "Cannot find required programs: ${MISSING_PROGS[@]}"
+
+# we do not require array MISSING_PROGS anymore; get rid of it
+unset MISSING_PROGS
