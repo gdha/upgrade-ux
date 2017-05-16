97daa9673ab25c3a9a1135345bedc59f845bead4
Gratien D'haese <gratien.dhaese@gmail.com>
2017-05-16 08:36:35 +0200

    16_check_nbu_backup.sh" replace bplist by bpps test

--- opt/upgrade-ux/scripts/prep/default/16_check_nbu_backup.sh
+++ opt/upgrade-ux/scripts/prep/default/16_check_nbu_backup.sh
@@ -2,9 +2,13 @@
 ########################
 # simple test to see if we are using NetBackup and if yes, we have a recent backup?
 
-[[ ! -x /usr/openv/netbackup/bin/bplist ]] && return   # no NBU sw installed - return
+[[ ! -x /usr/openv/netbackup/bin/bpps ]] && return   # no NBU sw installed - return
 
-LANG=C /usr/openv/netbackup/bin/bplist -l >/dev/null
+# bplist returns on Linux "EXIT STATUS 227: no entity was found". However, NBU is working fine.
+# LANG=C /usr/openv/netbackup/bin/bplist -l >/dev/null
+
+# bpps works rqual on HP-UX and Linux and is therefore a better test
+LANG=C /usr/openv/netbackup/bin/bpps >/dev/null
 if (( $? == 0 )) ; then
     Log "System is defined as NetBackup client and is being backup-ed"
 else
