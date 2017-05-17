a0c41dce71fa78e3d395f9823c1810c8d3315800
Gratien D'haese <gratien.dhaese@gmail.com>
2017-05-17 12:50:15 +0200

    fix rhn subscription after failed run

--- opt/upgrade-ux/scripts/postexecute/GNU/Linux/45_enable_rhn_satellite_subscription.sh
+++ opt/upgrade-ux/scripts/postexecute/GNU/Linux/45_enable_rhn_satellite_subscription.sh
@@ -1,9 +1,15 @@
 # postexecute/GNI/Linux/45_enable_rhn_satellite_subscription.sh
 # PREVIEW mode or not we moved it aside to be able to execute a patch check for updates
 
+# In some case when upgrade-ux was interrupted due to an error the systemid file was not moved back
+# to its original name, and therefore, in a 2th run we would assume there was no rhn subsription present.
+# However, before we started rhn subsription was there as file "$VAR_DIR/$DS/systemid.rhn" exists.
+# So, we check for this file and if present we make variable rhn_satellite_subscription=1
+[[ -f "$VAR_DIR/$DS/systemid.rhn" ]] && rhn_satellite_subscription=1
+
 if (( rhn_satellite_subscription )) ; then
     LogPrint "Re-enable RHN Satellite Subscription"
     [[ -f /etc/sysconfig/rhn/systemid.$DS ]] && mv -f /etc/sysconfig/rhn/systemid.$DS /etc/sysconfig/rhn/systemid && return
-    [[ -f "$VAR_DIR/$DS/systemid.rhn" ]] && mv -f "$VAR_DIR/$DS/systemid.rhn" /etc/sysconfig/rhn/systemid && return
+    [[ -f "$VAR_DIR/$DS/systemid.rhn" ]] && cp -f "$VAR_DIR/$DS/systemid.rhn" /etc/sysconfig/rhn/systemid && return
     LogPrint "WARNING: Could not re-enable redhat satellite subscription - do it manually"
 fi
