0badb657e76f109f68b11e6aa7e1f74edeebce96
Gratien D'haese <gratien.dhaese@gmail.com>
2017-05-17 17:30:04 +0200

    reworked the Error function for issue #68

--- opt/upgrade-ux/lib/input-output-functions.sh
+++ opt/upgrade-ux/lib/input-output-functions.sh
@@ -69,7 +69,8 @@ MASTER_PID=$$
 exec 7>&1
 QuietAddExitTask "exec 7>&-"
 # USR1 is used to abort on errors, not using Print to always print to the original STDOUT, even if quiet
-trap "echo 'Aborting due to an error, check $LOGFILE for details' >&7 ; kill $MASTER_PID" USR1
+# Move next line inside Error function as on Linux it did not work as expected (issue #68)
+#trap "echo 'Aborting due to an error, check $LOGFILE for details' >&7 ; kill $MASTER_PID" USR1
 
 
 # Check if any of the binaries/aliases exist
@@ -96,20 +97,10 @@ function Error {
     fi
     VERBOSE=1
     LogPrint "ERROR: $*"
-    if has_binary caller; then
-        # Print stack strace on errors in reverse order
-        (
-            echo "=== Stack trace ==="
-            c=0;
-            while caller $((c++)); do :; done | awk '
-                { l[NR]=$3":"$1" "$2 }
-                END { for (i=NR; i>0;) print "Trace "NR-i": "l[i--] }
-            '
-            echo "Message: $*"
-            echo "==================="
-        ) >&2
-    fi
-    kill -USR1 $MASTER_PID # make sure that Error exits the master process, even if called from child processes :-)
+    echo 'Aborting due to an error, check $LOGFILE for details' >&7
+    kill -QUIT $MASTER_PID
+    # Do not use USR1
+    #kill -USR1 $MASTER_PID # make sure that Error exits the master process, even if called from child processes :-)
 }
 
 function StopIfError {
--- opt/upgrade-ux/scripts/prep/default/05_reboot_required.sh
+++ opt/upgrade-ux/scripts/prep/default/05_reboot_required.sh
@@ -1,5 +1,5 @@
-# 5_reboot_required.sh
-# if the uptime of the system is higher then 30 days then we propose to reboot first
+# prep/default/05_reboot_required.sh
+# If the uptime of the system is higher then 30 days then we propose to reboot first
 
 # pre-define daysup as 0 (we can check this variable later [after a reboot])
 daysup=0
@@ -15,6 +15,7 @@ if (( daysup > DAYSUPTIME )) ; then
     if (( PREVIEW )) ; then
         LogPrint "Uptime of system is $daysup (>$DAYSUPTIME days) - recommend to reboot before patching"
     else
-        Error "Uptime of system is $daysup (>$DAYSUPTIME days) - reboot is required before patching with $PRODUCT"
+        # unless we used the "-F" force flag we quit
+        (( FORCED )) || Error "Uptime of system is $daysup (>$DAYSUPTIME days) - reboot is required before patching with $PRODUCT"
     fi
 fi
