64be30d89ce3120f1e33aaac498c0488db92d751
Gratien D'haese <gratien.dhaese@gmail.com>
2017-08-23 14:46:22 +0200

    fix ksh == into =

--- opt/upgrade-ux/scripts/postexecute/GNU/Linux/44_restore_yum_enable_status.sh
+++ opt/upgrade-ux/scripts/postexecute/GNU/Linux/44_restore_yum_enable_status.sh
@@ -7,7 +7,7 @@
 OIFS=$IFS
 IFS=":"
 while read repofile enabled ; do
-    if [[ "$enabled" == "enabled=1" ]] ; then
+    if [[ "$enabled" = "enabled=1" ]] ; then
         # original this $repofile was in an enabled state; therefore, we enable it again
         # just to be sure
         repo=$( basename $repofile ) # remove the path from the name
