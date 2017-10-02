10ae8ddbca828f106b3e0237b15b927e4d291825
Gratien D'haese <gratien.dhaese@gmail.com>
2017-10-02 16:29:02 +0200

    check files before executing - issue #75

--- opt/upgrade-ux/scripts/postexecute/default/38_start_quest.sh
+++ opt/upgrade-ux/scripts/postexecute/default/38_start_quest.sh
@@ -1,11 +1,11 @@
 # 38_start_quest.sh
-[[ ! -f /opt/quest/umi/sbin/quest-owcimomd_init ]] && return
+(( PREVIEW )) && return
 
-if (( PREVIEW )) ; then
-    :
-else
+if [[ -f /opt/quest/qmxcm/sbin/qmxcmd_init ]] ; then
     LogPrint "Start Quest qmxcmd"
     /opt/quest/qmxcm/sbin/qmxcmd_init start >&2
+fi
+if [[ -f /opt/quest/umi/sbin/quest-owcimomd_init ]] ; then
     LogPrint "Start Quest owcimomd daemon"
     /opt/quest/umi/sbin/quest-owcimomd_init start >&2
 fi
