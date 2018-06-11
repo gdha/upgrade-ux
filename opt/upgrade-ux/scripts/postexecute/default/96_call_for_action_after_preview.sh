43e9d6268ecf2f3d80c35c791c867a37244cb635
Gratien D'haese <gratien.dhaese@gmail.com>
2018-06-11 13:50:27 +0200

    Repeat all ERRORs found during a preview run on STDOUT - issue #86

---
+++ opt/upgrade-ux/scripts/postexecute/default/96_call_for_action_after_preview.sh
@@ -0,0 +1,16 @@
+# postexecute/default/96_call_for_action_after_preview.sh
+# Purpose of this script is to show all the logged 'ERROR' mesasages
+# found the the LOGFILE
+
+if (( PREVIEW )) ; then
+    grep -q ERROR $LOGFILE || return  # no ERRORs found; silently return
+    echo "
+******************************************************************************
+WARNING - We found ERROR(s) during the PREVIEW run - please take care and retry
+
+"
+grep ERROR $LOGFILE
+echo"
+******************************************************************************"
+    
+fi
