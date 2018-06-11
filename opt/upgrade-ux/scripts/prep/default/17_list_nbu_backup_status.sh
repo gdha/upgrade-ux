6991f99636c89c6354756d734be98c240f92983f
Gratien D'haese <gratien.dhaese@gmail.com>
2018-06-11 10:59:41 +0200

    NBU: check also for VM snapshots #84

--- opt/upgrade-ux/scripts/prep/default/17_list_nbu_backup_status.sh
+++ opt/upgrade-ux/scripts/prep/default/17_list_nbu_backup_status.sh
@@ -2,17 +2,25 @@
 
 [[ ! -x /usr/openv/netbackup/bin/bpclimagelist ]] && return   # no NBU sw installed - return
 
-LANG=C /usr/openv/netbackup/bin/bpclimagelist  > "$VAR_DIR/$DS/netbackup_backup_list.output"
+BPARG=""
+LANG=C /usr/openv/netbackup/bin/bpclimagelist $BPARG  > "$VAR_DIR/$DS/netbackup_backup_list.output"
 
 if [[ ! -s "$VAR_DIR/$DS/netbackup_backup_list.output" ]] ; then
-    # If the file "$VAR_DIR/$DS/netbackup_backup_list.output" has size 0 then the client is
-    # not properly defined in the NBU master and no backups were found - this is not OK!
+    # If the file "$VAR_DIR/$DS/netbackup_backup_list.output" has size 0 then it could
+    # be that this client is using snapshots (for VM). We will try if with -client option
+    BPARG="-client $HOSTNAME"
+    LANG=C /usr/openv/netbackup/bin/bpclimagelist $BPARG > "$VAR_DIR/$DS/netbackup_backup_list.output"
+fi
+
+if [[ ! -s "$VAR_DIR/$DS/netbackup_backup_list.output" ]] ; then
+    # If the file "$VAR_DIR/$DS/netbackup_backup_list.output" has size 0 then this client
+    # is not properly defined in the NBU master and no backups were found - this is not OK!
     LogPrint "Warning: No Symantec NetBackup backups were found!"
 
 else
 
     # backups were found ; show the date of the last full backup
-    LASTFULL=$(LANG=C /usr/openv/netbackup/bin/bpclimagelist | grep Full | head -1 | cut -c1-10)
+    LASTFULL=$(LANG=C /usr/openv/netbackup/bin/bpclimagelist $BPARG | grep Full | head -1 | cut -c1-10)
     LASTFULLSEC=$(date +%s -d $LASTFULL)
     NOWSEC=$(date +%s)
 
