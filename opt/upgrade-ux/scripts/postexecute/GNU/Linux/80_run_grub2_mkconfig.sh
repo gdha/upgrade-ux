6affd869b1b6196ce0d21a568cfb359f0231ddeb
Gratien D'haese <gratien.dhaese@gmail.com>
2018-06-11 11:14:02 +0200

    better logging of grub2 entries #85

--- opt/upgrade-ux/scripts/postexecute/GNU/Linux/80_run_grub2_mkconfig.sh
+++ opt/upgrade-ux/scripts/postexecute/GNU/Linux/80_run_grub2_mkconfig.sh
@@ -1,6 +1,12 @@
 # postexecute/GNU/Linux/80_run_grub2_mkconfig.sh
 # Script will run grub2-mkconfig (or grub-mkconfig)
 
+if [[ -f /etc/grub2.cfg ]];  then
+    # Show the current entries before running grub2-mkconfig
+    Log "Before running grub2-mkconfig the entries are:"
+    awk -F\' '$1=="menuentry " {print $2}' /etc/grub2.cfg  >&2
+fi
+
 if (( PREVIEW )) ; then
     LogPrint "Running GRUB makeconfig script [not in preview mode]"
 else
@@ -13,5 +19,9 @@ else
         grub-mkconfig -o /boot/grub/grub.cfg >&2
         Log "The first 'menuentry' of the grub.cfg file is:"
         grep ^menuentry /boot/grub/grub.cfg | head -1 >&2
+    fi
+    if [[ -f /etc/grub2.cfg ]];  then
+        Log "After running grub2-mkconfig the entries are:"
+        awk -F\' '$1=="menuentry " {print $2}' /etc/grub2.cfg  >&2
     fi
 fi
