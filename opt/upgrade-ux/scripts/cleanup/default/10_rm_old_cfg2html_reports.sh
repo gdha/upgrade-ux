88d882675467518148e03851736e54a74b231755
Gratien D'haese <gratien.dhaese@gmail.com>
2018-06-11 11:03:44 +0200

    put -xdev option before all other options #83

--- opt/upgrade-ux/scripts/cleanup/default/10_rm_old_cfg2html_reports.sh
+++ opt/upgrade-ux/scripts/cleanup/default/10_rm_old_cfg2html_reports.sh
@@ -2,4 +2,4 @@
 [[ ! -d /var/log/cfg2html ]]  &&  return  # no cfg2html log dir; just return
 
 Log "Removing old cfg2html data files (> 30 days old)"
-/usr/bin/find /var/log/cfg2html  -type f -xdev -mtime +30 -print -exec rm -f {} \; >&2
+/usr/bin/find /var/log/cfg2html -xdev  -type f -mtime +30 -print -exec rm -f {} \; >&2
