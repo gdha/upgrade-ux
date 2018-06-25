73086e92b22bb53ec8f4cbeb6d84ca98e2b101b7
Gratien D'haese <gratien.dhaese@gmail.com>
2018-06-25 12:03:29 +0200

    check if ntpd is running - to avoid error messages in log when ntpd is not running

--- opt/upgrade-ux/scripts/postexecute/GNU/Linux/25_start_ntpd.sh
+++ opt/upgrade-ux/scripts/postexecute/GNU/Linux/25_start_ntpd.sh
@@ -1,7 +1,9 @@
 # postexecute/GNU/Linux/25_start_ntpd.sh
+[ -n "$(pidof -x ntpd)" ] || return  # no ntpd running
+
 if (( PREVIEW )) ; then
     :
 else
-    LogPrint "Start ntp daemon"
+    LogPrint "Start ntpd daemon"
     service ntpd status || service ntpd start >&2
 fi
--- opt/upgrade-ux/scripts/preinstall/GNU/Linux/27_stop_ntpd.sh
+++ opt/upgrade-ux/scripts/preinstall/GNU/Linux/27_stop_ntpd.sh
@@ -1,5 +1,7 @@
 # 27_stop_ntp.sh
 # We stop ntpd services to avoid problems with the docker filesystems - known issue
+[ -n "$(pidof -x ntpd)" ] || return  # no ntpd running
+
 if (( PREVIEW )) ; then
     Log "Stopping ntpd daemon [not done in preview]"
 else
