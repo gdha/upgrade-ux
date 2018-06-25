03da8787153215ee875193197b8b2f6befe3eb60
Gratien D'haese <gratien.dhaese@gmail.com>
2018-06-25 11:48:39 +0200

    avoid empty docker_info.after file when no docker is installed (in the log file)

--- opt/upgrade-ux/scripts/postexecute/GNU/Linux/28_start_docker.sh
+++ opt/upgrade-ux/scripts/postexecute/GNU/Linux/28_start_docker.sh
@@ -3,9 +3,12 @@
 if (( PREVIEW )) ; then
     :
 else
-    LogPrint "Start docker service"
-    service docker status || service docker start >&2
-    # we shall not start docker containers automatically as these may be under control
-    # of a Rancher Manager
-    docker info > "$VAR_DIR/$DS/docker_info.after"
+    Log "Are we using docker?"
+    service docker status >&2 && {
+        LogPrint "Start docker service"
+        service docker status || service docker start >&2
+        # we shall not start docker containers automatically as these may be under control
+        # of a Rancher Manager
+        docker info > "$VAR_DIR/$DS/docker_info.after"
+    }
 fi
