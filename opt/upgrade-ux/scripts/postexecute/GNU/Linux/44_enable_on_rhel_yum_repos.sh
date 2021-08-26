f0e93d3fffb5a8ca03c7d99052b989972b29f00c
gdhaese1 <gdhaese1@its.jnj.com>
2021-08-26 16:01:36 +0200

    #119 using the correct repo name

--- opt/upgrade-ux/scripts/postexecute/GNU/Linux/44_enable_on_rhel_yum_repos.sh
+++ opt/upgrade-ux/scripts/postexecute/GNU/Linux/44_enable_on_rhel_yum_repos.sh
@@ -2,20 +2,19 @@
 # The purpose of this script to to enable all non RedHat related YUM repositories.
 # Why? Most likely these were disabled by another script (outside upgrade-ux) which
 # installed the new patch repo's and in the meantime disabled all present repo's.
-# Heaven knows why....
 
 [[ ! -d /etc/yum.repos.d ]]  &&  return  # no YUM repository directory present
 
-ls /etc/yum.repos.d/*.repo | grep -v -i -E '(rhel|redhat|testing)' | cut -d/ -f4 | while read repofile
+ls /etc/yum.repos.d/*.repo | grep -v -i -E '(rhel|redhat|testing)' | while read repofile
 do
     # input will look like:
-    # epel.repo
-    # gtsc.repo
-    repo=${repofile%.*}   # cut off .repo from string
+    # /etc/yum.repos.d/jnj-repos.repo
+    # repo=${repofile%.*}   # cut off .repo from string
+    repo=$(grep '^\[' $repofile | sed -e 's/\[//' -e 's/]//')
     Log "Enable non-RedHat related repository $repo"
     if has_binary yum-config-manager ; then
         yum-config-manager --enable $repo >&2
     else
-        sed -i -e "s|^enabled=*|enabled=1|" /etc/yum.repos.d/$repofile
+        sed -i -e "s|^enabled=*|enabled=1|" $repofile
     fi
 done
