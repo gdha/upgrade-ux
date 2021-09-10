03d2593d11168d0ac24f7999945f577fe00ddc17
gdhaese1 <gdhaese1@its.jnj.com>
2021-09-10 11:57:06 +0200

    correct the stop/start seq of ds_agent on Linux

--- opt/upgrade-ux/scripts/postexecute/GNU/Linux/20_start_ds_agent.sh
+++ opt/upgrade-ux/scripts/postexecute/GNU/Linux/20_start_ds_agent.sh
@@ -3,5 +3,6 @@ if (( PREVIEW )) ; then
     :
 else
     LogPrint "Start Trend Micro Deep Security anti-virus services"
-    [[ -x /etc/rc.d/init.d/ds_agent ]] && /etc/rc.d/init.d/ds_agent start >&2
+    # [[ -x /etc/rc.d/init.d/ds_agent ]] && /etc/rc.d/init.d/ds_agent start >&2
+    service ds_agent start >&2
 fi
--- opt/upgrade-ux/scripts/preinstall/GNU/Linux/10_stop_ds_agent.sh
+++ opt/upgrade-ux/scripts/preinstall/GNU/Linux/10_stop_ds_agent.sh
@@ -1,12 +1,15 @@
 # 10_stop_ds_agent.sh
 # Stop The Trend Micro Deep Security sub-system before patching
 
-[[ ! -f /etc/rc.d/init.d/ds_agent ]] && return  # not present
+service ds_agent status >&2 || return
+
+# old-style: [[ ! -f /etc/rc.d/init.d/ds_agent ]] && return  # not present
 
 if (( PREVIEW )) ; then
     Log "Stopping Trend Micro Deep Security services [not done in preview]"
 else
     LogPrint "Stopping Trend Micro Deep Security services"
-    [[ -x /etc/rc.d/init.d/ds_agent ]] && /etc/rc.d/init.d/ds_agent stop >&2
+    # [[ -x /etc/rc.d/init.d/ds_agent ]] && /etc/rc.d/init.d/ds_agent stop >&2
+    service ds_agent stop >&2
 fi
 
