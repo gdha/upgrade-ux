b802b3352ec38cfb61d647b640a8fd3e6af0d718
gdhaese1 <gdhaese1@its.jnj.com>
2021-10-11 12:22:10 +0200

    import RPM GPG keys before patching #127

---
+++ opt/upgrade-ux/scripts/preinstall/GNU/Linux/09_import_rpm_gpg_keys.sh
@@ -0,0 +1,11 @@
+# 09_import_rpm_gpg_keys.sh
+# To forecome issues with RPM GPG keys not yet being imported which could break upgrade-ux (issue #127)
+
+if test $RPM ; then
+    if (( PREVIEW )) ; then
+        Log "Running rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-* [not in preview mode]"
+    else
+        Log "Running rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-*"
+        rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-*
+    fi
+fi
