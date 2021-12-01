1b7dd666c203e9eff0b5c576c08357856a93508a
Gratien D'haese <gratien.dhaese@gmail.com>
2021-12-01 16:23:55 +0100

    moved the remove 30_install_bundles.sh script to preinstall.sh #128

--- packaging/HP-UX/preinstall.sh
+++ packaging/HP-UX/preinstall.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 # preinstall.sh
 
-/usr/bin/echo "       * preinstall.sh starts"
+echo "       * preinstall.sh starts"
+
+# remove old /opt/upgrade-ux/scripts/install/hp/30_install_bundles.sh script as it has been
+# renamed to /opt/upgrade-ux/scripts/install/hp/40_install_bundles.sh (by issue #98)
+[[ -f /opt/upgrade-ux/scripts/install/hp/30_install_bundles.sh ]] && rm -f /opt/upgrade-ux/scripts/install/hp/30_install_bundles.sh
+echo "       * Removed old /opt/upgrade-ux/scripts/install/hp/30_install_bundles.sh script"
 
