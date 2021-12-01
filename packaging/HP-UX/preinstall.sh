28da90e558a0308ea46b38cd8900baddd9c30200
Gratien D'haese <gratien.dhaese@gmail.com>
2021-12-01 16:29:18 +0100

    made some better if-blocks #128 in postremove.sh and preinstall.sh

--- packaging/HP-UX/preinstall.sh
+++ packaging/HP-UX/preinstall.sh
@@ -5,6 +5,7 @@ echo "       * preinstall.sh starts"
 
 # remove old /opt/upgrade-ux/scripts/install/hp/30_install_bundles.sh script as it has been
 # renamed to /opt/upgrade-ux/scripts/install/hp/40_install_bundles.sh (by issue #98)
-[[ -f /opt/upgrade-ux/scripts/install/hp/30_install_bundles.sh ]] && rm -f /opt/upgrade-ux/scripts/install/hp/30_install_bundles.sh
-echo "       * Removed old /opt/upgrade-ux/scripts/install/hp/30_install_bundles.sh script"
-
+if [[ -f /opt/upgrade-ux/scripts/install/hp/30_install_bundles.sh ]]; then
+    rm -f /opt/upgrade-ux/scripts/install/hp/30_install_bundles.sh
+    echo "       * Removed old /opt/upgrade-ux/scripts/install/hp/30_install_bundles.sh script"
+fi
