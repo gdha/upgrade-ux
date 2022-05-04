160a54f73dc1104adc68776dceab09e8072392dc
gdhaese1 <gdhaese1@its.jnj.com>
2022-05-04 15:41:31 +0200

    #133 add dnf.log comparison next to the yum.log

--- opt/upgrade-ux/scripts/postinstall/GNU/Linux/39_save_and_diff_yum_log.sh
+++ opt/upgrade-ux/scripts/postinstall/GNU/Linux/39_save_and_diff_yum_log.sh
@@ -1,14 +1,28 @@
 # 39_save_and_diff_yum_log.sh
 
 # we will always save a copy when we are almost done (to be sure)
-cp /var/log/yum.log "$VAR_DIR/$DS/yum.log.after"
+if [[ -f /var/log/yum.log ]] ; then
+   cp /var/log/yum.log "$VAR_DIR/$DS/dnf.log.after"
+fi
+if [[ -f /var/log/dnf.log ]] ; then
+   cp /var/log/dnf.log "$VAR_DIR/$DS/dnf.log.after"
+fi
 
 # if we do not have a 'before' copy we cannot compare (should never be the case)
-[[ ! -f "$VAR_DIR/$DS/yum.log.before" ]] && return
+[[ ! -f "$VAR_DIR/$DS/yum.log.before" && ! -f "$VAR_DIR/$DS/dnf.log.before" ]] && return
 
-cmp -s "$VAR_DIR/$DS/yum.log.after" "$VAR_DIR/$DS/yum.log.before"
-if ((  $? == 1 )); then
-    # Log to logfile only (otherwise too much info on the screen going by)
-    Log "The differences in yum.log are:"
-    diff "$VAR_DIR/$DS/yum.log.after" "$VAR_DIR/$DS/yum.log.before"  | grep '^<' | cut -c2- >&2
+if [[ -f "$VAR_DIR/$DS/yum.log.after" ]] ; then
+   cmp -s "$VAR_DIR/$DS/yum.log.after" "$VAR_DIR/$DS/yum.log.before"
+   if ((  $? == 1 )); then
+      # Log to logfile only (otherwise too much info on the screen going by)
+      Log "The differences in yum.log are:"
+      diff "$VAR_DIR/$DS/yum.log.after" "$VAR_DIR/$DS/yum.log.before"  | grep '^<' | cut -c2- >&2
+   fi
+elif [[ -f "$VAR_DIR/$DS/dnf.log.after" ]] ; then
+   cmp -s "$VAR_DIR/$DS/dnf.log.after" "$VAR_DIR/$DS/dnf.log.before"
+   if ((  $? == 1 )); then
+      # Log to logfile only (otherwise too much info on the screen going by)
+      Log "The differences in dnf.log are:"
+      diff "$VAR_DIR/$DS/dnf.log.after" "$VAR_DIR/$DS/dnf.log.before"  | grep '^<' | cut -c2- >&2
+   fi
 fi
--- opt/upgrade-ux/scripts/prep/GNU/Linux/39_save_yum_log.sh
+++ opt/upgrade-ux/scripts/prep/GNU/Linux/39_save_yum_log.sh
@@ -1,5 +1,12 @@
 # 39_save_yum_log.sh
 # Keep a copy of the latest yum.log file
 
-[[ -f "$VAR_DIR/$DS/yum.log.before" ]] && return  # if we have the yum.log file then there is no need to copy it again
-cp  /var/log/yum.log "$VAR_DIR/$DS/yum.log.before"
+if [[ -f /var/log/yum.log ]] ; then
+   [[ -f "$VAR_DIR/$DS/yum.log.before" ]] && return  # if we have the yum.log file then there is no need to copy it again
+   cp /var/log/yum.log "$VAR_DIR/$DS/yum.log.before"
+fi
+
+if [[ -f /var/log/dnf.log ]] ; then
+   [[ -f "$VAR_DIR/$DS/dnf.log.before" ]] && return  # if we have the dnf.log file then there is no need to copy it again
+   cp /var/log/dnf.log "$VAR_DIR/$DS/dnf.log.before"
+fi
