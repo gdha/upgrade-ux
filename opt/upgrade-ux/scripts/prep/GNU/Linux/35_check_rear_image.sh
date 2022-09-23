98268648c13e6b7803798ce4f64a8c5a1c86a8c4
gdha <gdhaese1@its.jnj.com>
2022-09-23 16:30:53 +0200

    Detect hardware platform on Linux with dmidecode #135

--- opt/upgrade-ux/scripts/prep/GNU/Linux/35_check_rear_image.sh
+++ opt/upgrade-ux/scripts/prep/GNU/Linux/35_check_rear_image.sh
@@ -1,10 +1,23 @@
 # 35_check_rear_image.sh script will try to find out if a RELAX-AND-RECOVER image was made before the upgrade
+
+# We will try to figure out if this system needs ReaR at all as AWS, GCP and Azure do not require it
+if test $DMIDECODE ; then
+    system_product=$($DMIDECODE -s system-product-name)
+    case $system_product in
+        ("Google Compute Engine") return ;; # Google
+        ("Virtual Machine") return ;; #Azure
+        ("HVM domU") return ;; # AWS RHEL/CentOS 7
+        ("*.*") return ;; # all kind of AWS types
+    esac
+fi
+
 if [[ ! -d /var/log/rear ]]; then
     LogPrint "WARNING: please install disaster recovery software before patching e.g. Relax-and-Recover (rear)"
+    return
 fi
 
 if [[ ! -f /var/log/rear/rear-$HOSTNAME.log ]]; then
-    LogPrint "WARNING: please create a disaster recovery image before patching with e.g. Relax-and-Recover (rear)"
+    LogPrint "WARNING: please run Relax-and-Recover (rear) before patching!"
 else
     Log "Version installed of $(rear -V)"
     Log "Saving /var/log/rear/rear-$HOSTNAME.log"
