aa119a26b5bdbb0d4ded6102dc182d921b32bf8d
gdha <gdhaese1@its.jnj.com>
2024-04-23 11:19:48 +0200

    Avoid false alerts from a missing Control-M agent #148
    Signed-off-by: gdha <gdhaese1@its.jnj.com>

--- opt/upgrade-ux/scripts/postexecute/GNU/Linux/26_start_ctmagent.sh
+++ opt/upgrade-ux/scripts/postexecute/GNU/Linux/26_start_ctmagent.sh
@@ -2,6 +2,7 @@
 if (( PREVIEW )) ; then
     :
 else
+    [[ -f /opt/TIDAL/BMC/installsoftware/ctm/exe/p_ctmag ]] || return  # no Ctrl-M present so silently return
     LogPrint "Start Control M daemon"
     service ctmagent status || service ctmagent start >&2
 fi
