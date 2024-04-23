120196a5baba3f659887530682e03421f68e6d61
gdha <gdhaese1@its.jnj.com>
2024-04-23 11:27:37 +0200

    avoid false alerts from a missing Tidal agent #149
    Signed-off-by: gdha <gdhaese1@its.jnj.com>

--- opt/upgrade-ux/scripts/postexecute/GNU/Linux/25_start_tidal.sh
+++ opt/upgrade-ux/scripts/postexecute/GNU/Linux/25_start_tidal.sh
@@ -2,6 +2,7 @@
 if (( PREVIEW )) ; then
     :
 else
+    [[ -f /etc/systemd/system/tagent.service ]] || return  # no Tidal systemd script present; just return
     LogPrint "Start tidal daemon"
     service tagent status || service tagent start >&2
 fi
