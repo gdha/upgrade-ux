2dd99d7bdc6044bba6f8c1d45f015992a119ffd5
Gratien D'haese <gratien.dhaese@gmail.com>
2024-06-10 11:08:11 +0200

    postinstall/default/10_save_dmesg.sh ksh -n complained about ==
    Signed-off-by: Gratien D'haese <gratien.dhaese@gmail.com>

--- opt/upgrade-ux/scripts/postinstall/default/10_save_dmesg.sh
+++ opt/upgrade-ux/scripts/postinstall/default/10_save_dmesg.sh
@@ -1,7 +1,7 @@
 # 10_save_dmesg.sh
 
 # Linux has its own 10_save_dmesg.sh script using 'dmesg -T'
-[[ "$OS" == "GNU/Linux" ]] && return
+[[ "$OS" = "GNU/Linux" ]] && return
 
 # for the non-Linux systems we use the standard command:
 dmesg > $VAR_DIR/$DS/dmesg.output
