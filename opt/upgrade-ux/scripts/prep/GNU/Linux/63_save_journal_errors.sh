2c6c0888602e39c34bbfbb2d63c02b0ac883e342
gdha <gratien.dhaese@gmail.com>
2024-07-15 14:30:18 +0200

    #172 fix wrong variable name
    Signed-off-by: gdha <gratien.dhaese@gmail.com>

--- opt/upgrade-ux/scripts/prep/GNU/Linux/63_save_journal_errors.sh
+++ opt/upgrade-ux/scripts/prep/GNU/Linux/63_save_journal_errors.sh
@@ -3,5 +3,5 @@
 # This may proof that some services were already showing errors before we patched.
 # To avoid discussions after patching...
 if type -p journalctl >/dev/null 2>&1 ; then
-   journalctl -p err --since today > "$VAR/$DS/journal_errors_today.before"
+   journalctl -p err --since today > "$VAR_DIR/$DS/journal_errors_today.before"
 fi
