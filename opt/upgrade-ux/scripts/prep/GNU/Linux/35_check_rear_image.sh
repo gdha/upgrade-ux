3bc5c315d8ec08d0ef3314760605755ea2c63b52
gdha <GDHAESE1@ITS.JNJ.COM>
2025-02-10 11:27:18 +0100

    #177 add a proper VIRTUAL check

--- opt/upgrade-ux/scripts/prep/GNU/Linux/35_check_rear_image.sh
+++ opt/upgrade-ux/scripts/prep/GNU/Linux/35_check_rear_image.sh
@@ -1,15 +1,20 @@
 # 35_check_rear_image.sh script will try to find out if a RELAX-AND-RECOVER image was made before the upgrade
 
+VIRTUAL=false
+
 # We will try to figure out if this system needs ReaR at all as AWS, GCP and Azure do not require it
 if test $DMIDECODE ; then
     system_product=$($DMIDECODE -s system-product-name)
     case $system_product in
-        ("Google Compute Engine") return ;; # Google
-        ("Virtual Machine") return ;; #Azure
-        ("HVM domU") return ;; # AWS RHEL/CentOS 7
-        ("*.*") return ;; # all kind of AWS types
+        ("Google Compute Engine") VIRTUAL=true ;; # Google
+        ("Virtual Machine") VIRTUAL=true ;; # Azure
+        ("HVM domU") VIRTUAL=true ;; # AWS RHEL/CentOS
+        ("*.*") VIRTUAL=true ;; # all kind of AWS types
     esac
 fi
+
+# If the system is a virtual system then there is no need to check for ReaR backups
+[[ "$VIRTUAL" == "true" ]] && return
 
 if [[ ! -d /var/log/rear ]]; then
     LogPrint "WARNING: please install disaster recovery software before patching e.g. Relax-and-Recover (rear)"
